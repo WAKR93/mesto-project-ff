b09b443cfd4ae77ed192e189e7debd482e4a88e0
Valery Kraft <WAKR93@yandex.ru>
2025-05-09 20:44:44 +0700

    на 1ю проверку

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/WAKR93/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const cardOnline = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-
+function addCard (dataCard) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const buttonRemove = cardElement.querySelector('.card__delete-button');
+  cardElement.querySelector('.card__image').src = dataCard.link;
+  cardElement.querySelector('.card__title').textContent = dataCard.name;  
+  buttonRemove.addEventListener('click', function (evt) {
+    removeCard(cardElement);
+  });
+  return cardElement;
+}
 // @todo: Функция удаления карточки
-
+const removeCard = function (element) {
+ element.remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (element) {
+  cardOnline.append(addCard(element));
+});
+ 
+  
